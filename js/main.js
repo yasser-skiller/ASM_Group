7abfd64bb42b7e349358a88bf11b5a57ebd14d0b
Yasser A. Gamal <63634166+yasser-skiller@users.noreply.github.com>
2023-06-11 18:40:30 +0300

    up

--- js/main.js
+++ js/main.js
@@ -1,61 +1,51 @@
-
-
 // if(document.querySelector('.navigat')){
     let navItem = document.querySelectorAll('.main-header .navigation li a');
-let navItem_arr = Array.from(navItem);
-
-window.addEventListener('load',()=>{
-    navItem_arr.forEach(ele => {
-        ele.classList.remove('current')
-       if(window.location.href ===  ele.href){
-        ele.parentElement.classList.add('current')
-       }
-    });
-})
-// }
-
-let Num = 3
-if(window.innerWidth > 767){
-    Num = 3
-}else{
-    Num = 2
-}
-
-if(document.querySelector('.mySwiper')){
-    var swiper = new Swiper("#id .mySwiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        loop: true,
-        centeredSlides: true,
-        slidesPerView: Num,
-        coverflowEffect: {
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-        },
-      });
-}
-
-// language
-window.addEventListener('load', ()=>{
-    if(localStorage.getItem('lang') == null){
-        localStorage.setItem('lang', 'ar')
+    let navItem_arr = Array.from(navItem);
+    
+    window.addEventListener('load',()=>{
+        navItem_arr.forEach(ele => {
+            ele.classList.remove('current')
+           if(window.location.href ===  ele.href){
+            ele.parentElement.classList.add('current')
+           }
+        });
+    })
+    // }
+    
+    let Num = 3
+    if(window.innerWidth > 767){
+        Num = 3
+    }else{
+        Num = 2
     }
-})
+    
+    if(document.querySelector('.mySwiper')){
+        var swiper = new Swiper("#id .mySwiper", {
+            effect: "coverflow",
+            grabCursor: true,
+            loop: true,
+            centeredSlides: true,
+            slidesPerView: Num,
+            coverflowEffect: {
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            },
+            pagination: {
+              el: ".swiper-pagination",
+            },
+          });
+    }
+    
+    // change language
+    document.querySelector("#langInput").addEventListener('change',()=>{
+        window.location.href = window.location.pathname + '?asm-lang=' + document.querySelector("#langInput").value;
+    })
 
-document.querySelector("#langInput").addEventListener('change',()=>{
-    localStorage.setItem('lang', `${document.querySelector("#langInput").value}` )
 
-})
-
-
-
-
+    
 //  // // // // // // // // // // // // // // // // // // // // // // 
 let Area = document.querySelector(`#Area`).value
 let PathRoom = document.querySelector(`#PathRoom`).value
