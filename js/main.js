730e50f9bb09c11f2e2dff1e493f01b29f796331
Yasser A. Gamal <63634166+yasser-skiller@users.noreply.github.com>
2023-06-11 18:38:37 +0300

    up

--- js/main.js
+++ js/main.js
@@ -52,3 +52,75 @@ document.querySelector("#langInput").addEventListener('change',()=>{
     localStorage.setItem('lang', `${document.querySelector("#langInput").value}` )
 
 })
+
+
+
+
+//  // // // // // // // // // // // // // // // // // // // // // // 
+let Area = document.querySelector(`#Area`).value
+let PathRoom = document.querySelector(`#PathRoom`).value
+let UnitKind = document.querySelector(`#UnitKind`).value
+let level = document.querySelector(`#level`).value
+let levelTwo = `الكلاسيك`
+
+
+// المقايسات
+let almuqayasat = document.querySelector(`#almuqayasat`).value = Area * 25
+
+// >رسومات تنفذية 
+let althaalith = document.querySelector(`#althaalith`).value = Area * 40
+
+
+// tanfidh
+let tanfidh = document.querySelector(`#tanfidh`).value = Area * 4000
+
+// > فرش 
+let firsh = document.querySelector(`#firsh`).value = Area * 2000
+
+
+
+
+
+// ألتصميم الثلاثي الابعاد ليه اكتر من خطوه للحساب
+
+// 1
+let stepOne =  Area * 75
+
+// 2
+let stepTwo =  PathRoom * 2500
+
+// 3
+
+let x
+if(UnitKind == 'وحدة سكنية'){
+    x = 1
+}else{
+    x=1.15
+}
+let stepThree =  (stepOne + stepTwo ) * x
+
+// 4
+
+let y
+if(level == 'متوسط'){
+    y = 1
+}else if (level == 'لوكس'){
+    y=1.1
+}else if (level == 'الترا لوكس'){
+    y = 1.2
+}
+let stepFour =  stepThree * y
+
+
+// 5
+
+let z
+if(levelTwo == 'المودرن'){
+    z = 1
+}else{
+    z=1.2
+}
+let stepFive =  stepFour * z
+
+// > 3d تصميم
+let Design3 = document.querySelector(`#Design3`).value = stepFive
